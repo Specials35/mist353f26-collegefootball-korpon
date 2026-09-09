91f5438676ea70e9a6cf4b5c10720d53784ddb01
AJ <ajkorpon@gmail.com>
2026-09-08 20:51:05 -0400

    Added N. Surendra as user and master user

--- Data/CreateTables.sql
+++ Data/CreateTables.sql
@@ -1,5 +1,22 @@
+
+CREATE LOGIN NandaSurendra
+WITH PASSWORD = 'MI$T353Instructor';
+
+CREATE USER NandaSurendra
+
+FOR LOGIN NandaSurendra;
+
+ALTER ROLE db_owner ADD MEMBER NandaSurendra;
+
+
+
+
+
+/*
 CREATE table test_table (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     created_at TIMESTAMP --DEFAULT CURRENT_TIMESTAMP
 );
+
+*/
